099702984d47b3e2308c2e8d801b3aa5d85c7b30
pohjanjoki <77720618+pohjanjoki@users.noreply.github.com>
2021-03-17 12:45:24 +0200

    17.3.2021

---
+++ test_laskenta.py
@@ -0,0 +1,8 @@
+# Moduulin laskenta.py testit
+
+# Moduulien ja kirjastojen lataukset
+import laskenta
+
+# Ensimmäinen testi: lasketaan painoindeksi ja verrataan tulosta ennalta laskettuun arvoon
+def test_painoindeksi():
+    assert laskenta.bmi(74, 1.71) == 25.3069320474676
--- test_sanity2.py
+++ test_sanity2.py
@@ -3,6 +3,20 @@
 # Modulin lataus
 import sanity2
 
-# Arvo rajojen sisällä, virhekoodi 0, virhesanoma Arvo OK
-def test_rajatarkistus():
+# Arvo rajojen sisällä, virhekoodi 0, virhesanoma 'Arvo OK'
+def test_rajatarkistus_oikein():
     assert sanity2.rajatarkistus(100, 20, 300) == [0, 'Arvo OK'] 
+
+# Arvo alle alarajan, virhekoodi 1, virhesanoma 'Arvo on alle alarajan (20)'
+def test_rajatarkistus_alle():
+    assert sanity2.rajatarkistus(10, 20, 300) == [1, 'Arvo on alle alarajan (20)'] 
+
+# Arvo yli ylärajan, virhekoodi 2, virhesanoma 'Arvo on yli ylärajan (300)'
+def test_rajatarkistus_yli():
+    assert sanity2.rajatarkistus(350, 20, 300) == [2, 'Arvo on yli ylärajan (300)']
+
+# LIUKULUKUMUUNNOS TESTIT
+
+
+
+
